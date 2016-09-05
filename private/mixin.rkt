e8b0cc8cffc3bf449769b04f065ebb825d8bdd70
Georges Dupéron <georges.duperon@gmail.com>
2016-09-05 23:23:32 +0200

    Use phc-toolkit's compat functions for racket v6.5

--- private/mixin.rkt
+++ private/mixin.rkt
@@ -2,6 +2,7 @@
 
 (require "no-order.rkt"
          (for-syntax syntax/parse
+                     phc-toolkit/untyped
                      racket/syntax))
 
 (provide ~mixin)
@@ -9,7 +10,6 @@
 (define-eh-mixin-expander ~mixin
   (syntax-parser
     [(_ (~var mixin (static eh-mixin-expander? "an eh-mixin expander")))
-     (with-disappeared-uses
-      (begin
-        (record-disappeared-uses #'mixin)
-        #'(mixin)))]))
+     (with-disappeared-uses*
+      (record-disappeared-uses* #'mixin)
+      #'(mixin))]))
